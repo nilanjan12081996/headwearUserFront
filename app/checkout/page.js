8b35818dc5f88030f8b61ecf92e00366192f4a79
unknown <nilanjandasgupta0@gmail.com>
2025-12-11 17:28:32 +0530

    msg

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Avatar, AvatarGroup, AvatarGroupCounter, Label, Select, FileInput, Checkbox, TextInput } from "flowbite-react";
 
@@ -70,20 +70,38 @@ import { FaPlus } from "react-icons/fa";
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { addAddress } from '../reducers/CheckoutSlice';
+import OrderSummary from './OrderSummary';
 
 
 
 
 
 const page = () => {
+    const [sameAddress, setSameAddress] = useState(false);
   const dispatch = useDispatch()
   const {
     register,
     handleSubmit,
+    watch,
+    setValue,
     formState: { errors },
   } = useForm();
 
   const savedUUid = sessionStorage.getItem("uuid")
+
+  const billing = watch("billing");
+
+  useEffect(() => {
+  if (sameAddress) {
+    setValue("shipping.line1", billing?.line1);
+    setValue("shipping.line2", billing?.line2);
+    setValue("shipping.city", billing?.city);
+    setValue("shipping.state", billing?.state);
+    setValue("shipping.postal_code", billing?.postal_code);
+    setValue("shipping.country", billing?.country);
+  }
+}, [sameAddress, billing, setValue]);
+
 
   const onSubmit = (data) => {
     const payload = {
@@ -295,7 +313,10 @@ const page = () => {
                 </div>
 
                 <div className="flex items-center gap-2 check_area mb-2">
-                  <Checkbox id="promotion" />
+                  <Checkbox id="promotion" 
+                   checked={sameAddress}
+                   onChange={(e) => setSameAddress(e.target.checked)}
+                  />
                   <Label className='text-[#615E5E] text-base' htmlFor="promotion">Ship to Same address</Label>
                 </div>
 
@@ -381,84 +402,7 @@ const page = () => {
                 </div>
               </div>
             </form>
-            <div className='lg:w-4/12 border border-[#E6E6E6] rounded-[10px] p-4'>
-              <h3 className='text-[22px] font-semibold text-[#1A1A1A] pb-4'>Order Summery</h3>
-
-              <div>
-
-                <div className='mb-4 flex items-center gap-2'>
-                  <div className='w-10/12 flex items-center gap-1'>
-                    <div className='w-3/12'>
-                      <Image src={small_cap} alt='small_cap' className='' />
-                    </div>
-                    <div className='w-9/12'>
-                      <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>Wooly Combed Black x48</p>
-                      <div className='flex items-center gap-4'>
-                        <p className='text-[12px] text-[#1A1A1A]'>Size: S</p>
-                        <p className='text-[12px] text-[#1A1A1A]'>Color: Black</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className='w-2/12'>
-                    <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>$70.00</p>
-                  </div>
-                </div>
-
-                <div className='mb-4 flex items-center gap-2'>
-                  <div className='w-10/12 flex items-center gap-1'>
-                    <div className='w-3/12'>
-                      <Image src={small_cap} alt='small_cap' className='' />
-                    </div>
-                    <div className='w-9/12'>
-                      <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>Wooly Combed Black x48</p>
-                      <div className='flex items-center gap-4'>
-                        <p className='text-[12px] text-[#1A1A1A]'>Size: S</p>
-                        <p className='text-[12px] text-[#1A1A1A]'>Color: Black</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className='w-2/12'>
-                    <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>$70.00</p>
-                  </div>
-                </div>
-
-                <div className='mb-4 flex items-center gap-2'>
-                  <div className='w-10/12 flex items-center gap-1'>
-                    <div className='w-3/12 flex justify-center items-center'>
-                      <CgFileDocument className='text-[#ed1c24] text-3xl' />
-                    </div>
-                    <div className='w-9/12'>
-                      <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>Artwork Setup/Digitizing x96</p>
-                    </div>
-                  </div>
-                  <div className='w-2/12'>
-                    <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>$120.00</p>
-                  </div>
-                </div>
-
-              </div>
-
-              <div>
-                <div className='flex items-center justify-between border-b border-[#e9e9e9] py-2 mb-2'>
-                  <p className='text-[14px] text-[#1A1A1A] pb-1 font-normal'>Subtotal:</p>
-                  <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>$260.00</p>
-                </div>
-                <div className='flex items-center justify-between border-b border-[#e9e9e9] py-2 mb-2'>
-                  <p className='text-[14px] text-[#1A1A1A] pb-1 font-normal'>Subtotal:</p>
-                  <p className='text-[14px] text-[#1A1A1A] pb-1 font-medium'>$260.00</p>
-                </div>
-                <div className='flex items-center justify-between py-2 mb-2'>
-                  <p className='text-[18px] text-[#1A1A1A] pb-1 font-normal'>Total:</p>
-                  <p className='text-[18px] text-[#1A1A1A] pb-1 font-bold'>$260.00</p>
-                </div>
-              </div>
-
-              <button className='bg-[#ED1C24] hover:bg-black text-white text-base rounded-full w-full py-3 cursor-pointer'>
-                Get Quote
-              </button>
-
-
-            </div>
+           <OrderSummary/>
           </div>
 
         </div>
